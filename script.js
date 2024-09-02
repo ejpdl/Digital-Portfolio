85daee0960fe9f7a382a682e1f4b8aa5e8e5dbc2
Ephraim De Lara <delaraephraim171@gmail.com>
2024-09-02 23:21:20 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".auto-typed", {
+
+    strings: ["Ephraim De Lara", "21 Years Old", "Male"],
+    typeSpeed : 60,
+    backSpeed : 60,
+    loop: true
+
+});
+
+const menu = document.querySelector("#menu-icon");
+const nav = document.querySelector(".menus");
+
+menu.addEventListener("click", () => {
+
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("show");
+
+})
+
