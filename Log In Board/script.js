c37d305e1f8e97c82f68113c6f442ca7769ac40a
Ephraim De Lara <delaraephraim171@gmail.com>
2024-09-04 19:02:14 +0800

    new folder

---
+++ Log In Board/script.js
@@ -0,0 +1,10 @@
+const typedElement = document.querySelector(".typed");
+
+const typed = new Typed(typedElement, {
+
+    strings: ["A digital portfolio", "exclusively for grade 7 only", "a prototype portfolio"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+
+});
